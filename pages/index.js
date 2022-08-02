682af5a243a5e5de515a85a378f4ec72e6cb6867
Luke Yamasaki <lukeyamasaki@gmail.com>
2022-08-01 18:21:02 -0600

    Added video and audio

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,37 @@
 import Head from 'next/head';
 import { SunDog } from '../components/SunDog';
-import { SunDogBack } from '../components/SunDog';
 import styles from '../styles/Home.module.css';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    setUrl("/music/surprise.mp3");
+    return
+  }, [])
+
   return (
-    <div className="container">
+    <div className={styles.main}>
       <Head>
         <title>WTF Lab Rat?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <audio autoPlay loop src={url} />
+      <div className={styles.videoContainer}>
+        <video
+          className={styles.video}
+          src={require('../public/videos/sunset.mp4')}
+          autoPlay
+          loop
+        />
+      </div>
 
       <main className={styles.bg}>
         <div className={styles.vectorBox}>
           <SunDog />
-          <SunDogBack />
         </div>
       </main>
-
     </div>
   )
 }
